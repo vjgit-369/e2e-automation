7c4df11e75b5e79853f9952ff282a1680b5d3b56
vjgit-369 <vjbigdata369@gmail.com>
2025-06-02 21:15:35 +0530

    Configure headless mode via environment variable

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   use: {
-    headless: true,
+    headless: process.env.PLAYWRIGHT_HEADLESS === 'true',
     viewport: { width: 1280, height: 720 },
     actionTimeout: 30000,
     navigationTimeout: 30000
